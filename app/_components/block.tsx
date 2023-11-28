751f48d973879bf8af3a87979a45644d2e4ddf99
nohr <aite@nyu.edu>
2023-11-27 21:52:49 -0600

    fix block animation

---
+++ app/_components/block.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { urlFor } from "./url";
+import { ProjectType, WordType } from "../app";
+import { PortableText } from "@portabletext/react";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import { useCallback, useRef } from "react";
+
+export default function Block({ data }: { data: ProjectType | WordType }) {
+  const blockRef = useRef<HTMLDivElement>(null!);
+  const randomHeight = useCallback(
+    () => `${Math.floor(Math.random() * 200) + 250}px`,
+    [],
+  );
+  const pathname = usePathname();
+  const active = pathname.includes(data.slug.current);
+
+  return (
+    <motion.div
+      whileInView={{ height: randomHeight() }}
+      viewport={{
+        once: true,
+      }}
+      ref={blockRef}
+      className={`relative inline-flex aspect-square h-auto w-full items-center justify-center overflow-hidden duration-300 ease-in-out ${
+        data._type !== "project" && " !inline-block border"
+      } ${active ? "pointer-events-none border" : ""}`}
+    >
+      <Link className="contents" href={`/${data._type}s/${data.slug.current}`}>
+        {data._type === "project" && (
+          <>
+            <Image
+              src={data.image ? urlFor(data.image).url() : ""}
+              alt={data.title || ""}
+              sizes="100vw, (min-width: 1024px) 33vw, 50vw"
+              fill
+              suppressHydrationWarning
+              blurDataURL={data.image ? urlFor(data.image).blur(1).url() : ""}
+              className={`absolute z-10 object-cover hover:opacity-25 ${
+                active ? "opacity-10" : ""
+              }`}
+            />
+            <h2 className=" text-current p-2 text-3xl">{data.title}</h2>
+          </>
+        )}
+
+        {data._type !== "project" && (
+          <div
+            onScroll={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+            }}
+            onWheel={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+            }}
+            className={`words animate-autoscroll overflow-y-scroll text-4xl hover:opacity-25 [&_>_p]:pointer-events-none ${
+              active ? "opacity-10" : ""
+            }`}
+          >
+            <PortableText value={data.text} />
+          </div>
+        )}
+      </Link>
+    </motion.div>
+  );
+}
--- app/_components/grid.tsx
+++ app/_components/grid.tsx
@@ -1,14 +1,7 @@
 "use client";
-
-import Image from "next/image";
-import Link from "next/link";
-import { urlFor } from "./url";
-import { BlockType, ProjectType, WordType } from "../app";
-import { PortableText } from "@portabletext/react";
+import { BlockType } from "../app";
 import { usePathname } from "next/navigation";
-import { useInView } from "framer-motion";
-import { useRef } from "react";
-import { act } from "react-dom/test-utils";
+import Block from "./block";
 
 export default function Grid({
   children,
@@ -38,50 +31,5 @@ export default function Grid({
         </div>
       )}
     </>
-  );
-}
-
-function Block({ data }: { data: ProjectType | WordType }) {
-  const blockRef = useRef(null!);
-  const isInView = useInView(blockRef, {
-    once: false,
-  });
-  const randomHeight = `${Math.floor(Math.random() * 200) + 250}px`;
-  const pathname = usePathname();
-  const active = pathname.includes(data.slug.current);
-  
-  return (
-    <Link
-      ref={blockRef}
-      className={`w-full inline-flex items-center justify-center relative overflow-hidden h-auto aspect-square duration-300 ease-in-out ${
-        data._type !== "project" && " border !inline-block"
-      } ${active ? "pointer-events-none border" : ""}`}
-      href={`/${data._type}s/${data.slug.current}`}
-      style={{ height: isInView ? randomHeight : randomHeight }}
-    >
-      {data._type === "project" && (
-        <>
-          <Image
-            src={data.image ? urlFor(data.image).url() : ""}
-            alt={data.title || ""}
-            sizes="100vw, (min-width: 1024px) 33vw, 50vw"
-            fill
-            suppressHydrationWarning
-            blurDataURL={data.image ? urlFor(data.image).blur(1).url() : ""}
-            className={`absolute object-cover hover:opacity-25 z-10 ${active ? "opacity-10" : ""}`}
-          />
-          <h2 className=" p-2 text-3xl text-current">{data.title}</h2>
-        </>
-      )}
-
-      {data._type !== "project" && (
-        <div
-          onScroll={(e) => { e.stopPropagation(); e.preventDefault(); }}
-          onWheel={(e) => { e.stopPropagation(); e.preventDefault(); }}
-          className={`words animate-autoscroll hover:opacity-25 overflow-y-scroll [&_>_p]:pointer-events-none text-4xl ${active ? "opacity-10": ""}`}>
-          <PortableText value={data.text} />
-        </div>
-      )}
-    </Link>
   );
 }
